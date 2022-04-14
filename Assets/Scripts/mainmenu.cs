3fe77b887fb479153bfa042125674a2d3bff4ac3
Abdelghaffar Nasser <58223290+AbdelghaffarNasser@users.noreply.github.com>
2022-04-14 02:44:32 +0200

    first commit

---
+++ Assets/Scripts/mainmenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+public class mainmenu : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    // Update is called once per frame
+    public void Quit()
+    {
+        Debug.Log("Quit");
+
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/play.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+public class play : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    public void click()
+    {
+       
+        SceneManager.LoadScene("Main Menue");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
